f99fa546fadd183f5091113f49f46fc041c0f016
nxbsolution <nexusberry.solution@gmail.com>
2024-12-30 23:37:11 +0500

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,10 +6,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 
-// import { Categories } from './collections/Categories'
 import { Media } from './collections/Media'
-// import { Pages } from './collections/Pages'
-// import { Posts } from './collections/Posts'
 import { Users } from './collections/Users'
 import { Footer } from './Footer/config'
 import { Header } from './Header/config'
@@ -64,7 +61,7 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI || '',
     },
   }),
-  collections: [Pages, Posts, Media, Categories, Users],
+  collections: [Media, Users],
   cors: [getServerSideURL()].filter(Boolean),
   globals: [Header, Footer],
   plugins: [
