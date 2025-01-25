2c663a8cf9a081340d6a46906723d8f222144080
M-Ali-Kaleem <alikaleem189@gmail.com>
2025-01-25 23:41:35 +0500

    resolved shadcn and tailwind errors

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,10 @@
-export default function Page(){
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
   return (
     <div>
-      <h1 className="bg-red-500 text-center text-2xl text-yellow-500">Page</h1>
+      <h1 className="bg-red-500 text-yellow-500 p-5 font-bold text-center text-2xl">Page</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
