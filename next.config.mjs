bf593b214b2a986b9ff082d065434941fa70ceda
M-Ali-Kaleem <alikaleem189@gmail.com>
2025-02-03 12:58:47 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://teamofninjas.vercel.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' }
+        ]
+      }
+    ];
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '4mb',
